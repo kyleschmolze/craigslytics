e5730efe9fc588c0601939b62ceb48fc489bce18
Kyle Nichols-Schmolze <kyletns@gmail.com>
2013-06-06 15:57:08 -0400

    done with overview part of show page

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -14,6 +14,7 @@ class AnalysesController < ApplicationController
   # GET /analyses/1.json
   def show
     @analysis = Analysis.find(params[:id])
+    @body_class = 'analysis-show'
 
     respond_to do |format|
       format.html # show.html.erb
