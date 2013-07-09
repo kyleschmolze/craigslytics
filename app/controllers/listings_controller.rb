3304e39dd21dd63b43a601bfc67bdcf0e3083b62
Sam Dushay <sdushay@gmail.com>
2013-07-09 15:07:40 -0400

    Todd feed now

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,9 @@
 class ListingsController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.where(user_id: current_user.id)
+    @listings = Listing.where(user_id: 1)
     @geocoded_address = nil
     # If bedrooms is set, 
     #   only grab listings with that number of bedrooms
